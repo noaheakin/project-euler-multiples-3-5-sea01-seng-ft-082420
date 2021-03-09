1c5aeaf22823dad5daeda973278caf3d2f72f08e
noaheakin <noaheakin@gmail.com>
2021-03-08 18:41:20 -0800

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,9 @@
-# Enter your procedural solution here!
+def collect_multiples(n)
+    (3...n).select{|i| (i % 5 == 0 || i % 3 == 0)}
+end
+
+puts collect_multiples(1000).inject(:+)
+
+def sum_multiples(n)
+    collect_multiples(n).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
-# Enter your object-oriented solution here!
+class Multiples 
+    attr_reader :limit, :multiples
+
+    def initialize(limit)
+        @limit = limit
+        @multiples = collect_multiples
+    end
+
+    def collect_multiples
+        (3...limit).to_a.reject{|n| n unless n % 3 == 0 || n % 5 == 0}
+    end
+
+    def sum_multiples
+        multiples.inject(:+)
+    end
+end
